bff8a48cd914d4fe15c72bdf70f003ca63df5ab4
AdamWindows <adamkimmerer@2erdesign.com>
2014-07-03 17:08:55 -0400

    First
    First

---
+++ RPS Studios/Index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>RPS Studios Inc.</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<link rel="stylesheet/less" type="text/css" href="style.less" />
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic' rel='stylesheet' type='text/css'/>
+	<link href='http://fonts.googleapis.com/css?family=Arvo:400,400italic' rel='stylesheet' type='text/css'/>
+	<script type="text/javascript" src="jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+	<script type="text/javascript" src="modernizr.custom.59412.js"></script>
+	<script src="http://lesscss.googlecode.com/files/less-1.0.30.min.js"></script>
+	<script></script>
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Projects</a></li>
+				<li id="logo"><img src="Images/logo.png"></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+		</nav>
+	</header>
+	<section id="screen">
+		<h1 id="main">Lorem Ipsum Dolor</h1>
+		<p id="secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+		 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+		 dolor in reprehenderit in voluptate velit esse cillum</p>
+		 <h2 id="start" class="blink_me">Press to Start</h2>
+	</section>
+	<section id="arcade">
+		<div id="top"></div>
+			<div id="stick">
+			</div>
+			<div id="greenbutton">
+			</div>
+			<div id="redbutton">
+			</div>
+			<div id="paper">
+			</div>
+			<div id="vivi">
+			</div>
+			<div id="link">
+			</div>
+			<div id="invader">
+			</div>
+			<div id="ship"></div>
+			<div id="laser"></div>
+	</section>
+	<section id="mid">
+		<div id="case">
+		</div>
+		<div id="bandaid">
+		</div>
+		<div id="leftwheel">
+		</div>
+		<div id="rightwheel">
+		</div>
+		<div id="mouse"></div>
+	</section>
+</body>
+</html>
